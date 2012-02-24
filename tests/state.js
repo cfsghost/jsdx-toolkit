84a01fd0a71b425503a41997bd8a76d83a822180
Fred Chien <fred@mandice.com>
2012-02-24 16:22:35 +0800

    change window title

--- tests/state.js
+++ tests/state.js
@@ -7,7 +7,7 @@ if (clutter.init() != clutter.INIT_SUCCESS) {
 
 /* Create a new stage */
 var stage = new clutter.Stage();
-stage.setTitle('Animate');
+stage.setTitle('State Manchine');
 stage.resize(500, 500);
 stage.setColor(0, 0, 0, 255);
 stage.on(clutter.EVENT_DESTROY, function() {
