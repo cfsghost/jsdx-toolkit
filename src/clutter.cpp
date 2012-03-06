456435695e6891960be11eaea312aee4d7d65d33
Fred Chien <fred@mandice.com>
2012-03-06 16:23:53 +0800

    replace clutter_main() with g_main_context to avoid process hangging

--- src/clutter.cpp
+++ src/clutter.cpp
@@ -1,5 +1,7 @@
 #include <v8.h>
 #include <node.h>
+#include <ev.h>
+#include <stdlib.h>
 #include <clutter/clutter.h>
 #include <clutter/x11/clutter-x11.h>
 
@@ -27,7 +29,134 @@ namespace clutter {
  
 using namespace node;
 using namespace v8;
- 
+
+struct econtext {
+	GPollFD *pfd;
+	ev_io *iow;
+	int nfd, afd;
+	gint maxpri;
+
+	ev_prepare pw;
+	ev_check cw;
+	ev_timer tw;
+
+	GMainContext *gc;
+};
+
+static struct econtext default_context;
+
+static void timer_cb(EV_P_ ev_timer *w, int revents) {
+  /* nop */
+}
+
+static void io_cb(EV_P_ ev_io *w, int revents) {
+  /* nop */
+}
+
+static void prepare_cb(EV_P_ ev_prepare *w, int revents)
+{
+	struct econtext *ctx = (struct econtext *)(((char *)w) - offsetof (struct econtext, pw));
+	gint timeout;
+	int i;
+
+	g_main_context_dispatch(ctx->gc);
+
+	g_main_context_prepare(ctx->gc, &ctx->maxpri);
+
+	while(ctx->afd < (ctx->nfd = g_main_context_query(
+		ctx->gc,
+		ctx->maxpri,
+		&timeout,
+		ctx->pfd,
+		ctx->afd))) {
+
+		free (ctx->pfd);
+		free (ctx->iow);
+
+		ctx->afd = 1;
+		while (ctx->afd < ctx->nfd)
+			ctx->afd <<= 1;
+
+		ctx->pfd = (GPollFD*) malloc(ctx->afd * sizeof (GPollFD));
+		ctx->iow = (ev_io*) malloc(ctx->afd * sizeof (ev_io));
+	}
+
+	for (i = 0; i < ctx->nfd; ++i) {
+		GPollFD *pfd = ctx->pfd + i;
+		ev_io *iow = ctx->iow + i;
+
+		pfd->revents = 0;
+
+		ev_io_init(iow,
+			io_cb,
+			pfd->fd,
+			(pfd->events & G_IO_IN ? EV_READ : 0)
+			| (pfd->events & G_IO_OUT ? EV_WRITE : 0));
+
+		iow->data = (void *)pfd;
+		ev_set_priority(iow, EV_MINPRI);
+		ev_io_start(EV_A_ iow);
+	}
+
+	if (timeout >= 0) {
+		ev_timer_set(&ctx->tw, timeout * 1e-3, 0.);
+		ev_timer_start(EV_A_ &ctx->tw);
+	}
+}
+
+static void check_cb(EV_P_ ev_check *w, int revents)
+{
+	struct econtext *ctx = (struct econtext *)(((char *)w) - offsetof (struct econtext, cw));
+	int i;
+
+	for (i = 0; i < ctx->nfd; ++i) {
+		ev_io *iow = ctx->iow + i;
+
+		if (ev_is_pending(iow)) {
+			GPollFD *pfd = ctx->pfd + i;
+			int revents = ev_clear_pending(EV_A_ iow);
+
+			pfd->revents |= pfd->events & ((revents & EV_READ ? G_IO_IN : 0) | (revents & EV_WRITE ? G_IO_OUT : 0));
+		}
+
+		ev_io_stop(EV_A_ iow);
+	}
+
+	if (ev_is_active(&ctx->tw))
+		ev_timer_stop(EV_A_ &ctx->tw);
+
+	g_main_context_check(ctx->gc, ctx->maxpri, ctx->pfd, ctx->nfd);
+}
+
+static void ContextInit()
+{
+	GMainContext *gc = g_main_context_default();
+	struct econtext *ctx = &default_context;
+
+	if (!g_thread_supported())
+		g_thread_init(NULL);
+
+	g_main_context_acquire(gc);
+	ctx->gc = g_main_context_ref(gc);
+	ctx->nfd = 0;
+	ctx->afd = 0;
+	ctx->iow = 0;
+	ctx->pfd = 0;
+
+	ev_prepare_init(&ctx->pw, prepare_cb);
+	ev_set_priority(&ctx->pw, EV_MINPRI);
+	ev_prepare_start(EV_DEFAULT_UC_ &ctx->pw);
+	ev_unref(EV_DEFAULT_UC);
+
+	ev_check_init(&ctx->cw, check_cb);
+	ev_set_priority(&ctx->cw, EV_MAXPRI);
+	ev_check_start(EV_DEFAULT_UC_ &ctx->cw);
+	ev_unref(EV_DEFAULT_UC);
+
+	ev_init(&ctx->tw, timer_cb);
+	ev_set_priority(&ctx->tw, EV_MINPRI);
+} 
+
 static Handle<Value> ClutterInit(const Arguments& args)
 {
 	HandleScope scope;
@@ -37,7 +166,9 @@ static Handle<Value> ClutterInit(const Arguments& args)
 
 static Handle<Value> ClutterMain(const Arguments& args)
 {
-	clutter_main();
+	HandleScope scope;
+
+	ev_ref(EV_DEFAULT_UC);
 
 	return Undefined();
 }
@@ -54,6 +185,8 @@ static Handle<Value> SetUseARGBVisual(const Arguments& args)
 static Handle<Value> ClutterMainQuit(const Arguments& args)
 {
 	clutter_main_quit();
+
+	ev_unref(EV_DEFAULT_UC);
 
 	return Undefined();
 }
@@ -197,6 +330,8 @@ extern "C" {
 #if ENABLE_WIDGET
 		FlickView::Initialize(target);
 #endif
+
+		ContextInit();
 	}
 
 	NODE_MODULE(clutter, init);
